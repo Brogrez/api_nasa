c90aa06219d3e80bb274743353d6641d33d12d65
nico <nico.mv1998@gmail.com>
2023-07-18 14:56:11 -0400

    consuming api nasa to show image of the day

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ImgNasa />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImgNasa from "./components/ImgNasa.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImgNasa
   }
 }
 </script>
---
+++ src/components/ImgNasa.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img :src="nasaData.url" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+name:"Image_Nasa",
+data(){
+    return {
+        nasaData: {}
+    }
+},
+methods:{
+    async apiFetch(){
+        try{
+            let response = await fetch('https://api.nasa.gov/planetary/apod?api_key=NmsdTlDI4q2bfXFnXSsZETpkVlTLmH0HzkUPPAtd')
+            let json =  await response.json();
+            this.nasaData = json
+            console.log(this.nasaData)
+        } catch(error){
+            console.log(error)
+        }
+    }
+},
+created(){
+    this.apiFetch()
+}
+}
+
+</script>
+
+<style>
+
+</style>
